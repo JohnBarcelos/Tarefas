93d5d4a1b7ee1af3314ace3b1cfd7130daa8c923
JohnBarcelos <jonatanbarcelos14@gmail.com>
2019-02-02 15:34:52 -0200

    Mudança no menu

--- frontend/src/template/menu.jsx
+++ frontend/src/template/menu.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default props => (
-    <nav className='navbar navbar-inverse bg-inverse'>
+    <nav className='navbar navbar-default bg-default'>
         <div className='container'>
             <div className='navbar-header'>
                 <a className='navbar-brand' href="#">
